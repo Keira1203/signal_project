8e815a78977475bde189de24cad376cbb6ec8e75
Keira <k.nishigori@student.maastrichtuniversity.nl>
2026-04-29 18:58:38 +0200

    Improvement from week1 feedback

--- src/main/java/com/cardio_generator/generators/AlertGenerator.java
+++ src/main/java/com/cardio_generator/generators/AlertGenerator.java
@@ -13,8 +13,8 @@ import com.cardio_generator.outputs.OutputStrategy;
  */
 public class AlertGenerator implements PatientDataGenerator {
 
-    public static final Random randomGenerator = new Random();
-    private boolean[] AlertStates; // false = resolved, true = pressed
+    private static final Random RANDOM_GENERATOR = new Random();
+    private boolean[] alertStates; // false = resolved, true = pressed
 
     /**
      * initialize all patients alert situation list
@@ -23,7 +23,7 @@ public class AlertGenerator implements PatientDataGenerator {
      *                     start with all false(non alert)
      */
     public AlertGenerator(int patientCount) {
-        AlertStates = new boolean[patientCount + 1];
+        alertStates = new boolean[patientCount + 1];
     }
 
     /**
@@ -39,19 +39,19 @@ public class AlertGenerator implements PatientDataGenerator {
     @Override
     public void generate(int patientId, OutputStrategy outputStrategy) {
         try {
-            if (AlertStates[patientId]) {
-                if (randomGenerator.nextDouble() < 0.9) { // 90% chance to resolve
-                    AlertStates[patientId] = false;
+            if (alertStates[patientId]) {
+                if (RANDOM_GENERATOR.nextDouble() < 0.9) { // 90% chance to resolve
+                    alertStates[patientId] = false;
                     // Output the alert
                     outputStrategy.output(patientId, System.currentTimeMillis(), "Alert", "resolved");
                 }
             } else {
-                double Lambda = 0.1; // Average rate (alerts per period), adjust based on desired frequency
-                double p = -Math.expm1(-Lambda); // Probability of at least one alert in the period
-                boolean alertTriggered = randomGenerator.nextDouble() < p;
+                double lambda = 0.1; // Average rate (alerts per period), adjust based on desired frequency
+                double p = -Math.expm1(-lambda); // Probability of at least one alert in the period
+                boolean alertTriggered = RANDOM_GENERATOR.nextDouble() < p;
 
                 if (alertTriggered) {
-                    AlertStates[patientId] = true;
+                    alertStates[patientId] = true;
                     // Output the alert
                     outputStrategy.output(patientId, System.currentTimeMillis(), "Alert", "triggered");
                 }
